45086107f29e805c2c77db1d85819c453eeb70fc
jinit24 <jinit245@gmail.com>
2022-04-10 15:16:20 +0530

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="NSEDownload",
-    version="4.1",
+    version="4.1.1",
     author="Jinit S",
     description="Download Stocks and Indices Data from NSE",
 
