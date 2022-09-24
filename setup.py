05b4ef0805c4e2587c8ba0649cb37e1d357edbe4
jinit24 <jinit24@users.noreply.github.com>
2022-09-24 08:39:16 +0000

    Bumping Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="NSEDownload",
-    version="4.1.9",
+    version="4.1.10",
     author="Jinit S",
     description="Download Stocks and Indices Data from NSE",
 
